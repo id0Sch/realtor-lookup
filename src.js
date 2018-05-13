0311146d6e591ed00b7029a1f9c7ef030259ce3e
Ido S <ido@arpeely.com>
2018-05-13 21:13:20 +0300

    initial commit

---
+++ src.js
@@ -0,0 +1,38 @@
+window.addEventListener('load', () => {
+})
+document.getElementById("form")
+  .addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const f = new FormData(e.target)
+    const val = f.get('a')
+
+    const list = document.getElementById('list')
+    list.innerHTML = ''
+
+    const rel = window.getRealtors()
+
+    const result = rel.filter(i => i.license.toString().indexOf(val) > -1 || i.name.indexOf(val) > -1)
+    // var options = {
+    //   shouldSort: true,
+    //   threshold: 1,
+    //   location: 0,
+    //   distance: 100,
+    //   maxPatternLength: 32,
+    //   minMatchCharLength: 1,
+    //   keys: [
+    //     "name",
+    //     "license"
+    //   ]
+    // };
+
+    // var fuse = new Fuse(rel, options); // "list" is the item array
+    // var result = fuse.search(val);
+    result.forEach(i => {
+      const item = document.createElement('li')
+      item.className = "list-group-item"
+      item.innerText = `${i.name} - ${i.license}`
+      list.appendChild(item)
+    })
+    return false
+  })
